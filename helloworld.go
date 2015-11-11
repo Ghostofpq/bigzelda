ee0958716d31abfe263abe56823d2198b46f033d
Ghostofpq <ghostofpq@gmail.com>
2015-11-11 09:59:37 +0100

    utilisation de logrus

--- helloworld.go
+++ helloworld.go
@@ -1,9 +1,11 @@
 package main
 
 import (
-	"os"
 	"fmt"
 	"net/http"
+	"os"
+
+	log "github.com/Sirupsen/logrus"
 	"github.com/gorilla/mux"
 	"gopkg.in/redis.v3"
 )
@@ -11,46 +13,50 @@ import (
 var redisClient *redis.Client
 
 func main() {
-    fmt.Println("hello boyyy")
+	log.Info("Starting up!")
 	InitRedisClient()
-	r := mux.NewRouter()
+	r := mux.NewRouter()	
+	log.Info("Registering GorillaHandler on /gorilla")
 	r.HandleFunc("/gorilla", GorillaHandler)
-    r.HandleFunc("/{value}", RedirectionHandler)
-    r.HandleFunc("/shortlink/{value}", ShortlinkCreationHandler)
-    r.HandleFunc("/admin/{value}", MonitoringHandler)
-    http.Handle("/", r)
+	log.Info("Registering RedirectionHandler on /{value}")
+	r.HandleFunc("/{value}", RedirectionHandler)
+	log.Info("Registering ShortlinkCreationHandler on /shortlink/{value}")
+	r.HandleFunc("/shortlink/{value}", ShortlinkCreationHandler)
+	log.Info("Registering MonitoringHandler on /admin/{value}")
+	r.HandleFunc("/admin/{value}", MonitoringHandler)
+	http.Handle("/", r)
+	log.Info("Starting up on :8000")
 	http.ListenAndServe(":8000", r)
 }
 
 func InitRedisClient() {
+	log.Info("Setting up Redis client")
 	redisClient := redis.NewClient(&redis.Options{
-        Addr:     "localhost:6379",
-        Password: "", // no password set
-        DB:       0,  // use default DB
-    })
+		Addr:     "localhost:6379",
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
 	pong, err := redisClient.Ping().Result()
-	if(pong == "PONG"){
-		fmt.Println("Redis started")
-	} else{
-		fmt.Println(err)		
+	if pong == "PONG" {
+		log.Info("Redis client is up and running")
+	} else {
+		log.WithFields(log.Fields{"err": err}).Fatal("Redis could not start")
 		os.Exit(1)
 	}
 }
 
 func GorillaHandler(w http.ResponseWriter, r *http.Request) {
-    w.Write([]byte("Gorilla!\n"))
-	pong, err := redisClient.Ping().Result()
-	fmt.Println(pong, err)
+	w.Write([]byte("Gorilla!\n"))
 }
 
 func RedirectionHandler(w http.ResponseWriter, r *http.Request) {
-    w.Write([]byte("RedirectionHandler!\n"))
+	w.Write([]byte("RedirectionHandler!\n"))
 }
 
 func ShortlinkCreationHandler(w http.ResponseWriter, r *http.Request) {
-    w.Write([]byte("ShortlinkCreationHandler!\n"))
+	w.Write([]byte("ShortlinkCreationHandler!\n"))
 }
 
 func MonitoringHandler(w http.ResponseWriter, r *http.Request) {
-    w.Write([]byte("MonitoringHandler!\n"))
+	w.Write([]byte("MonitoringHandler!\n"))
 }
