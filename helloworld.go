b164b7f8823043746131644444251bd45d88fc2b
Ghostofpq <ghostofpq@gmail.com>
2015-11-11 09:22:52 +0100

    gorilla is ouga bouga on 8000

--- helloworld.go
+++ helloworld.go
@@ -1,9 +1,23 @@
 package main
 
-import "fmt"
-import "time"
+import (
+"fmt"
+"net/http"
+"github.com/gorilla/mux"
+)
 
 func main() {
-    fmt.Println("hello world")
-    time.Sleep(3 * time.Second)
+    fmt.Println("hello boyyy")
+	r := mux.NewRouter()
+	r.HandleFunc("/gorilla", GorillaHandler)
+    //r.HandleFunc("/{value}", RedirectionHandler)
+    //r.HandleFunc("/shortlink/{value}", ShortlinkCreationHandler)
+    //r.HandleFunc("/admin/{value}", MonitoringHandler)
+    http.Handle("/", r)
+
+	http.ListenAndServe(":8000", r)
+}
+
+func GorillaHandler(w http.ResponseWriter, r *http.Request) {
+    w.Write([]byte("Gorilla!\n"))
 }
